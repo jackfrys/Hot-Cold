b65a64da195221c7606b632869daed63c8dd0e66
Jack Frysinger <jcf0810@me.com>
2015-02-07 14:03:30 -0500

    UIPickerView up and running.

--- HotCold/MainViewController.swift
+++ HotCold/MainViewController.swift
@@ -8,18 +8,68 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
     @IBOutlet weak var logoImageVIew: UIImageView!
     @IBOutlet weak var placeTypeLabel: UILabel!
     @IBOutlet weak var radiusLabel: UILabel!
     @IBOutlet weak var myPickerView: UIPickerView!
+    @IBOutlet weak var goButtonOutlet: UIButton!
+    
+    var placeTypeOptions = ["Restaurants", "Historical Landmarks", "Museums", "Parks", "Geocaches"]
+    var radiusOptions = [0.5, 1.0, 5.0, 10.0, 25.0, 50.0]
+    
+    var myPickerViewDataSource = [AnyObject]()
+    var lastSelectedPickerViewButton = UIButton()
+    
+    var placeTypeIndex = 0
+    var radiusIndex = 0
     
     @IBAction func openPickerView(sender: UIButton) {
-        
+        if (sender.currentTitle == "Finding:") {
+            self.myPickerViewDataSource = self.placeTypeOptions
+        } else {
+            self.myPickerViewDataSource = self.radiusOptions
+        }
+        if (self.lastSelectedPickerViewButton == sender) {
+            self.myPickerView.hidden = true
+            self.lastSelectedPickerViewButton = UIButton()
+        } else {
+            self.myPickerView.hidden = false
+            self.lastSelectedPickerViewButton = sender
+        }
+        self.myPickerView.reloadAllComponents()
+        self.goButtonOutlet.hidden = !self.myPickerView.hidden
+        print(self.myPickerViewDataSource)
     }
     
     @IBAction func goButton() {
         
+    }
+    
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.myPickerViewDataSource.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        print(self.myPickerViewDataSource[row])
+        return "\(self.myPickerViewDataSource[row])"
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.setIndex(row)
+    }
+    
+    func setIndex(index: Int) {
+        if (self.lastSelectedPickerViewButton.currentTitle == "Finding:") {
+            self.placeTypeIndex = index
+        } else {
+            self.radiusIndex = index
+        }
     }
 }
