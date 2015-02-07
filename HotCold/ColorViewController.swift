9bcd98a6b5808450a6ac35ffdb6435d0ac0bcef3
David Alelyunas <David@Davids-MacBook-Pro.local>
2015-02-07 01:08:44 -0500

    finished changeColor Method

--- HotCold/ColorViewController.swift
+++ HotCold/ColorViewController.swift
@@ -23,15 +23,47 @@ class ColorViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func determineColorChange(currentColor: UIColor) {
-       // if currentColor.get
+    func changeColor(currentColor: UIColor, posDirection:Bool ) -> UIColor {
+        
+        var r: CGFloat = 0, g: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
+        currentColor.getRed(&r, green: &g, blue: &b, alpha: &a)
+        
+        var curRed: CGFloat = r * CGFloat(255.0)
+        var curGreen: CGFloat = g * CGFloat(255.0)
+        var curBlue: CGFloat = b * CGFloat(255.0)
+
+        
+        if (posDirection) {
+            if (curBlue == 255.0 && curRed < 255.0) {
+                return UIColor(red: (curRed + 1.0) / 255.0, green: curGreen, blue: curBlue, alpha: a)
+            }
+            else if (curBlue > 0) {
+                return UIColor(red: curRed, green: curGreen, blue: (curBlue - 1.0) / 255.0, alpha: a)
+            }
+            else {
+                return currentColor
+            }
+            
+        }
+        else {
+            if(curRed == 255.0 && curBlue < 255.0) {
+                return UIColor(red: curRed, green: curGreen, blue: (curBlue + 1.0) / 255.0, alpha: a)
+            }
+            else if (curRed > 0) {
+                return UIColor(red: (curRed - 1.0) / 255.0, green: curGreen, blue: curBlue, alpha: a)
+            }
+            else {
+                return currentColor
+            }
+            
+        }
     }
     
-    func colder() {
+    func changeBlue(direction: Bool) {
         
     }
     
-    func warmer() {
+    func changeRed(direction: Bool) {
         colorView.backgroundColor = UIColor.redColor()
     }
     
