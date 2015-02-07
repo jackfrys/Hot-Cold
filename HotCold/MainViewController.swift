4842414bac4442286f82931b059a8f79c0d32a2a
Jack Frysinger <jcf0810@me.com>
2015-02-07 12:51:01 -0500

    Populated Storyboard with new elements and linked objects to the ViewController.

---
+++ HotCold/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  HotCold
+//
+//  Created by Jack Frysinger on 2/7/15.
+//  Copyright (c) 2015 Jack Frysinger. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var logoImageVIew: UIImageView!
+    @IBOutlet weak var placeTypeLabel: UILabel!
+    @IBOutlet weak var radiusLabel: UILabel!
+    @IBOutlet weak var myPickerView: UIPickerView!
+    
+    @IBAction func openPickerView(sender: UIButton) {
+        
+    }
+}
