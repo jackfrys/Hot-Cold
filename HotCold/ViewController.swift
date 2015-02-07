6dc7f9d9e77674e5d146e147f63db420c943290d
Jack Frysinger <jcf0810@me.com>
2015-02-07 01:01:21 -0500

    Got picker view wired up to an array of distances.

--- HotCold/ViewController.swift
+++ HotCold/ViewController.swift
@@ -8,14 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    let locationOptions = [0.5, 1.0, 5.0, 10.0, 25.0, 50.0]
     
     @IBAction func startButton() {
         
     }
+    
     @IBAction func selectRadius(sender: UIButton) {
         self.pickerView.hidden = false
     }
+    
     @IBOutlet weak var pickerView: UIPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +31,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
     
 
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.locationOptions.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return "\(self.locationOptions[row])"
+    }
 }
 
