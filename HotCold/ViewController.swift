313dfb1b9cfef6fad3211c44842f4b6e30d449df
Jack Frysinger <jcf0810@me.com>
2015-02-07 00:23:37 -0500

    Slider updates the text display.

--- HotCold/ViewController.swift
+++ HotCold/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var locationRadiusSlider: UISlider!
+    @IBOutlet weak var locationRadiusDisplay: UILabel!
+
+    @IBAction func sliderUpdated() {
+        self.updateDisplay()
+    }
+    
+    @IBAction func startButton() {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func updateDisplay() {
+        self.locationRadiusDisplay.text = "\(self.locationRadiusSlider.value)"
+    }
 
 }
 
