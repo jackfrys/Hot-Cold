43d415d5e8d9a9c4ba080dad7cbf35b1b8b5faa0
Jack Frysinger <jcf0810@me.com>
2016-11-10 18:23:04 -0500

    trimming shared location

--- HotCold/SharedLocation.swift
+++ HotCold/SharedLocation.swift
@@ -5,31 +5,15 @@
 //  Created by Charlie Peters on 2/7/15.
 //  Copyright (c) 2015 Jack Frysinger. All rights reserved.
 //
-// http://hc.milodavis.com/getLocation.php
 
 import Foundation
 import CoreLocation
-import UIKit
 
 class SharedLocation: NSObject, CLLocationManagerDelegate {
     
     static let sharedLocation = SharedLocation()
     
-    var controller: UIViewController?
-    
     var locationManager = CLLocationManager()
-    
-    // You can access the current lat and long by calling:
-    // currentLocation2d.latitude, etc
-    var currentLocation2d: CLLocationCoordinate2D?
-    
-    // You can access the absolute distance to the goal
-    dynamic var distance: CLLocationDistance
-    
-    // NOTE TO MILO, Change this if you are testing dummy locations
-    // Closest Chipotle: lat 42.362428, long -71.085611
-    let dummyLocationCoord: CLLocationCoordinate2D = CLLocationCoordinate2DMake(42.362428, -71.085611)
-    let dummyLocation: CLLocation
     
     var delegate : CLLocationManagerDelegate? {
         didSet {
@@ -40,16 +24,16 @@ class SharedLocation: NSObject, CLLocationManagerDelegate {
     private override init() {
         // Update every 5 meters
         self.locationManager.distanceFilter  = 5
+        
         // Accurate to 10 meters
         self.locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        
         // Start updating location if you already have permission
         self.locationManager.startUpdatingLocation()
+        
         // If you don't have permission, ask nicely (message in plist)!
-        print("requesting in init")
         self.locationManager.requestWhenInUseAuthorization()
-        // Test Location
-        self.dummyLocation = CLLocation(latitude: dummyLocationCoord.latitude, longitude: dummyLocationCoord.longitude)
-        self.distance = 100.0
+
         super.init()
         self.locationManager.delegate = self
     }
