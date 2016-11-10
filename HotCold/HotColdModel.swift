5b7790432fe56276b6c2c89db0c7f49e1f13286f
Jack Frysinger <jcf0810@me.com>
2016-11-10 01:04:17 -0500

    stubbed out model

---
+++ HotCold/HotColdModel.swift
@@ -0,0 +1,33 @@
+//
+//  HotColdModel.swift
+//  HotCold
+//
+//  Created by Jack Frysinger on 11/10/16.
+//  Copyright © 2016 Jack Frysinger. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HotColdModel {
+    
+    func backgroundColor() -> UIColor {
+        return UIColor()
+    }
+    
+    func destinationUrl() -> URL {
+        return URL(string: "")!
+    }
+    
+    func directiveText() -> String {
+        return ""
+    }
+    
+    func placeType(atIndex: Int) -> String {
+        return ""
+    }
+    
+    func startGame(forCategoryAtIndex: Int, radius: Double) {
+        
+    }
+}
